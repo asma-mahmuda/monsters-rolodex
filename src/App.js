56aaaecc7e8a7a56023dace568383ceb72b945fe
Asma <asmi.mha+1@gmail.com>
2022-08-31 07:04:25 +1000

    Adding monster array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters:[
+        {
+          id: '1',
+          name: 'Funky Monster'
+        },
+        {
+          id:'2',
+          name: 'Doodly Monster'
+        }
+      ]
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+       {
+        this.state.monsters.map((monsters)=> {
+        return <div key={monsters.id}> <h1>{monsters.name}</h1></div>;
+        })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
